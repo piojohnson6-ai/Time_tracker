b10b74f62ff34d669c4a3c7e94f39d9c853819ba
piojohnson6-ai <piojohnson@gmail.com>
2025-08-09 21:50:48 +0530

    ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Time_tracker/',    // <---- move this here, at top-level config
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
